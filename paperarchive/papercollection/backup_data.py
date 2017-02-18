69caf8ae186ce1349ceaaf5e94f5ff683d9a71d6
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2017-02-18 17:39:24 -0500

    Minor changes

--- paperarchive/papercollection/backup_data.py
+++ paperarchive/papercollection/backup_data.py
@@ -3,6 +3,14 @@ import sys, os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def scrub_elements(input_string):
+    """
+    This function removes leading and trailing white spaces,
+    trailing commas as these are the separators between
+    BibTex fields, quotes and curly brackets as the value
+    of BibTex fields can be enclosed within these. Also
+    removed are forward slashes as they are Latex commands
+    and the terminating new line character.
+    """
     if input_string:
         while input_string[0]==" ":
             input_string = input_string[1:]
@@ -42,18 +50,6 @@ def scrub_elements(input_string):
                 input_string = input_string[:count] + input_string[count+1:]
 
     if input_string:
-        while input_string[0]==" ":
-            input_string = input_string[1:]
-            if not input_string:
-                break
-
-    if input_string:
-        while input_string[-1]==" ":
-            input_string = input_string[:-1]
-            if not input_string:
-                break
-
-    if input_string:
         while input_string[-1]=="\n" or input_string[-1]=="\r":
             input_string = input_string[:-1]
             if not input_string:
@@ -63,6 +59,13 @@ def scrub_elements(input_string):
 
 
 def read_ref_file():
+    """
+    This file reads the references in a text file which are
+    in BibTex form. It checks for the fields that are present
+    in every publication and adds them to a dictionary. The
+    dictionary for each publication is added to a list.
+    """
+
     input_file = open(os.path.join(BASE_DIR, 'input_data_file.txt'), "r")
 
     collection_of_articles = []
@@ -73,8 +76,10 @@ def read_ref_file():
         line = scrub_elements(line)
 
         if line:
+            # The BibTex entry for a publication starts with
+            # @article or @inproceedings. So this is the
+            # indication of a new publication.
             if line[0]=="@":
-##                print "New entry"
                 if record:
                     collection_of_articles.append(record)
                 record = {}
@@ -83,51 +88,39 @@ def read_ref_file():
             new_item = line.split("=")
             if len(new_item)>1:
                 if scrub_elements(new_item[0][0:5]).lower()=="title":
-##                    print scrub_elements(new_item[1])
                     record["title"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:6]).lower()=="author":
-##                    print scrub_elements(new_item[1])
                     record["author"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:8]).lower()=="journal":
-##                    print scrub_elements(new_item[1])
                     record["journal"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:10]).lower()=="booktitle":
-##                    print scrub_elements(new_item[1])
                     record["booktitle"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:5]).lower()=="year":
-##                    print scrub_elements(new_item[1])
                     record["year"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:6]).lower()=="month":
-##                    print scrub_elements(new_item[1])
                     record["month"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:7]).lower()=="volume":
-##                    print scrub_elements(new_item[1])
                     record["volume"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:7]).lower()=="number":
-##                    print scrub_elements(new_item[1])
                     record["number"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:6]).lower()=="pages":
-##                    print scrub_elements(new_item[1])
                     record["pages"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:4]).lower()=="doi":
-##                    print scrub_elements(new_item[1])
                     record["doi"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:9]).lower()=="abstract":
-##                    print scrub_elements(new_item[1])
                     record["abstract"] = scrub_elements(new_item[1])
 
                 if scrub_elements(new_item[0][0:9]).lower()=="keywords":
-##                    print scrub_elements(new_item[1])
                     record["keywords"] = scrub_elements(new_item[1])
 
     return collection_of_articles
--- paperarchive/papercollection/views.py
+++ paperarchive/papercollection/views.py
@@ -11,7 +11,7 @@ def dbase_populate(request):
     collection_of_articles = backup_data.read_ref_file()
     return render_to_response("list_papers.html", \
                     {'collection_of_articles' : collection_of_articles}, 
-                      context_instance=RequestContext(request))
+                    context_instance = RequestContext(request))
 
 def dbase_display(request):
     return HttpResponse("Hello again")
